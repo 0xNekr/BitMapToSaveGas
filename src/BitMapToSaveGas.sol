dccf42fa9b20a716018a718b2dd2724bdf6d1888
0xNekr <0xNekr.dev@gmail.com>
2023-07-17 12:52:39 +0200

    remove: Counter files

---
+++ src/BitMapToSaveGas.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {BitMaps} from "@openzeppelin/contracts/utils/structs/BitMaps.sol";
+
+contract BitMapToSaveGas {
+
+    using BitMaps for BitMaps.BitMap;
+
+    constructor(){
+
+    }
+}
